95b4abbab921eb707cf03f9a509f9b27a52f86a1
Priyank Singh <48864580+priyanksingh02@users.noreply.github.com>
2020-12-20 19:59:46 +0530

    Update stats.sh: Include stress and spy runs

--- target_program/stats.sh
+++ target_program/stats.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/zsh
 
+dir="result100"
 # Stats for long running processes
 # $1 program name
 # $2 output file name
 function stats {
   ./$1 > /dev/null & local pid=$!
-  perf stat -e cache-references,cache-misses -p $pid -r 10 -o $2 --append sleep 1
-  perf stat -e cycles -p $pid -r 10 -o $2 --append sleep 1
-  perf stat -e instructions -p $pid -r 10 -o $2 --append sleep 1
-  perf stat -e page-faults -p $pid -r 10 -o $2 --append sleep 1
-  perf stat -e L1-dcache-misses -p $pid -r 10 -o $2 --append sleep 1
+  perf stat -e cache-references,cache-misses,LLC-prefetches,L1-dcache-misses -p $pid -r 100 -o $2 --append sleep 1
+  perf stat -e cycles,instructions,page-faults -p $pid -r 100 -o $2 --append sleep 1
   kill $pid
 }
 
-stats linear stats.txt
+mkdir $dir
+stats $1 "${dir}/${1}-stats.txt"
+echo "stress test"
+stress -m 1 -c 1 &
+sleep 1
+stats $1 "${dir}/${1}-stats-stress.txt"
+killall stress
+sleep 1
+echo "spy test"
+# check jle instruction using gdb -> 0x3192; this will change based on application
+../single_probe_spy/spy ./$1 0x3192 > /dev/null & spyid=$!
+stats $1 "${dir}/${1}-stats-spy.txt"
+kill $spyid
+echo "done"
+
+
